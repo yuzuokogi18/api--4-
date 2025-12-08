c018fc8565c593ac22dabc7756cc633283c2fe6d
kull18 <regiber.montoya2005@hotmail.com>
2025-12-08 14:48:39 -0600

    Update conn

--- src/main/kotlin/com.equipxpress/infrastructure/database/DatabaseFactory.kt
+++ src/main/kotlin/com.equipxpress/infrastructure/database/DatabaseFactory.kt
@@ -7,7 +7,7 @@ object DatabaseFactory {
     
     fun init() {
         // Configuración para PostgreSQL
-        val jdbcURL = "jdbc:postgresql://44.205.87.247:5432/equipxpress"
+        val jdbcURL = "jdbc:postgresql://35.174.134.181:5432/equipxpress"
         val driverClassName = "org.postgresql.Driver"
         val user = "app_user"
         val password = "AppUser#2025!"  // ← Cambia esto por tu contraseña real
